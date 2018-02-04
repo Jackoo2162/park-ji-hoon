a62f2668779a7c265db1ca42b67a64091b2a1c54
Jackoo2162 <35918848+Jackoo2162@users.noreply.github.com>
2018-02-04 16:29:00 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+
+
+
+bot.on('message', message => {
+  if (message.content === 'nico nico ni') {
+   // message.reply('pong');
+    message.channel.sendMessage('annyeong');
+  }
+});
+
+bot.on("ready", () => {
+  bot.user.setPresence({game: {name:"DRINKING WINE"}})
+  console.log('I have jackoo: {SERVERNAME}')
+})
+
+bot.on('ready', ()=>{
+  console.log(`Logged in as ${bot.user.tag}!`);
+  voiceChannel = bot.guilds.find('name', 'DREAMENERG3T1CER').channels.find('name', 'JI-HOON');
+  voiceChannel.join().then(()=>{
+      console.log(`Connected to ${voiceChannel.name}!`);
+  });
+});
+
+
+// Create an event listener for new guild members
+bot.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'general');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
+
+bot.login('NDA4MTkwMjMwNjM4ODg2OTEy.DVMzEw.NDj8PKx_dcx5k35UM93t3bBVQe0');
